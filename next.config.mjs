962fc864f368c4f340e27be2bc4e0b9d6e7ff0b0
Ritinpaul <ritin.pal125@gmail.com>
2026-06-30 01:24:29 +0530

    chore: wix integration

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://*.wix.com https://*.wixsite.com https://*.editorx.com",
+            value: "frame-ancestors 'self' https://*.wix.com https://*.wixsite.com https://*.editorx.com https://*.wixstudio.com",
           },
         ],
       },
